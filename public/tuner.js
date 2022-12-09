136d8312483fc9cb07b97364a04500cdc77a8058
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2022-12-09 10:43:11 -0500

    Metronome start

--- public/tuner.js
+++ public/tuner.js
@@ -3,15 +3,24 @@
 const lowestFreq = 27.5;
 // A9
 const highestFreq = 14080;
-const noteNames = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a']
+
+let tempo = 60;
+const minTempo = 30;
+const maxTempo = 252;
+
+const noteNames = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let analyserNode = audioCtx.createAnalyser()
 let localMaxima = new Array(10);
-const frequencyDisplayElement = document.getElementById('note');
+let metronomeOn = false;
+const noteDisplay = document.getElementById('note');
+const tempoDisplay = document.getElementById('tempo');
 const hand = document.getElementById('hand')
 
 const startPitchDetection = async () => {
     try {
+        metronomeOn = false;
         const stream = await navigator.mediaDevices.getUserMedia ({audio: true})
 
         let microphoneStream = audioCtx.createMediaStreamSource(stream);
@@ -26,30 +35,30 @@ const startPitchDetection = async () => {
             const sigSounds = audioData.filter((d) => Math.abs(d) > 0.015);
             if (sigSounds.length < 1) return;
 
-            let pitch = getAutocorrolatedPitch();
+            let pitch = getAutocorrolatedPitch(audioData);
 
             if (Number.isNaN(pitch) || pitch < lowestFreq || pitch > highestFreq ) return;
 
-            const index = Math.log2(pitch / lowestFreq) * 12 % 12;
+            const relNote = Math.log2(pitch / lowestFreq) * 12;
 
-            const note = noteNames[Math.round(index)];
+            const index = Math.round(relNote) % 12;
 
-            let rotate = 90 * (index - Math.round(index));
+            const note = noteNames[index];
+
+            let rotate = 45 * (relNote - Math.round(relNote));
+
+            console.log(rotate)
 
             const currRotation = hand.style.transform.match(/rotate\((.+)\)/);
 
             if (currRotation) {
                 const currDeg = currRotation.slice(1)[0].replace(/deg$/, '');
 
-                const sign = rotate < 0 ? -1 : 1;
-
-                rotate = sign * Math.abs(rotate - currDeg);
+                // plus degree because wole metronome is shifted -45deg
+                rotate = Math.abs(rotate + currDeg);
             }
 
-            console.log(rotate)
-
-
-            frequencyDisplayElement.innerHTML = `${note}`;
+            noteDisplay.innerHTML = `${note}`;
 
             hand.style.transform = `rotate(${rotate}deg)`;
         }, 600);
@@ -58,7 +67,7 @@ const startPitchDetection = async () => {
     };
 }
 
-const getAutocorrolatedPitch = () => {
+const getAutocorrolatedPitch = (audioData) => {
     // First: autocorrolate the signal
 
     let maximaCount = 0;
@@ -91,6 +100,46 @@ const getAutocorrolatedPitch = () => {
     return audioCtx.sampleRate / maximaMean;
 }
 
-const angle = (dist) => {
+const tap = () => {
+    if (metronomeOn) return;
+    metronomeOn = true;
+    // first rotate all the way right
+    const currRotation = hand.style.transform.match(/rotate\((.+)\)/);
+
+    if (currRotation) {
+        const currDeg = currRotation.slice(1)[0].replace(/deg$/, '');
+
+        hand.style.transform = `rotate(${ - currDeg}deg)`;
+    }
+
+    // then oscilate between left and right
+    let rotate = -45;
+    const met = setInterval(() => {
+        hand.style.transform = `rotate(${-45 + rotate}deg)`;
+        rotate *= -1;
+        // play sound
+        if (!metronomeOn) clearInterval(met);
+    }, (60 / tempo) * 1000);
+}
+
+
+const increaseTempo = () => {
+    const inc = Math.floor(tempo / 20);
+    if (tempo + inc > maxTempo) tempo = minTempo;
+    else tempo += inc;
+    hand.style.transitionDuration = `${60 / tempo}s`;
+    tempoDisplay.innerHTML = `${tempo}`;
+}
+
+const decreaseTempo = () => {
+    const dec = Math.floor(tempo / 20);
+    if (tempo + dec > minTempo) tempo = maxTempo;
+    else tempo -= dec;
+    hand.style.transitionDuration = `${60 / tempo}s`;
+    tempoDisplay.innerHTML = `${tempo}`;
+}
+
+
+const playNote = () => {
 
 }
